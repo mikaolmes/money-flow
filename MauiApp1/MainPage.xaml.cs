bcd9bfe4ceaa1bf037d2a9439d571b869dc21786
Mika Olmes <M.Olmes.inf23@stud.bbbaden.ch>
2025-08-29 16:56:51 +0200

    Design Added

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCurrencyButtonClicked(object sender, EventArgs e)
+        {
+            // Logik für Währungswechsel
+            CurrencyPopup.IsVisible = !CurrencyPopup.IsVisible;
+
+        }
+
+        private void OnCurrencySelected(object sender, EventArgs e)
+        {
+            if (sender is ImageButton btn && btn.Source is FileImageSource img)
+            {
+
+                (sender as ImageButton).IsEnabled = false;
+
+
+                (this.FindByName<ImageButton>("CurrencyButton")).Source = img.File;
+
+                CurrencyPopup.IsVisible = false;
+            }
+        }
+    }
+
+}
